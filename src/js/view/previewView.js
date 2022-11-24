776bd740c89738ffa12dc4f99f24a2206ee15f7d
DjurovicAleksandar <djur.aleksandar@gmail.com>
2022-11-24 16:32:44 +0100

    fracty instead fractional

--- src/js/view/previewView.js
+++ src/js/view/previewView.js
@@ -5,6 +5,7 @@ class PreviewView extends View {
   _parentElement = '';
 
   generateMarkup(res) {
+    console.log(res);
     const id = window.location.hash.slice(1);
 
     return `
@@ -13,7 +14,7 @@ class PreviewView extends View {
               id === res.id ? ' preview__link--active' : ''
             }" href="#${res.id}">
 					<figure class="preview__fig">
-								<img src="${res.imageUrl}" alt="${res.title}" />
+								<img src="${res.image}" alt="${res.title}" />
 							</figure>
 						<div class="preview__data">
 								<h4 class="preview__title">${res.title}</h4>
